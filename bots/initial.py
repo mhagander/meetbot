f4def4953625b9232414910615570ac99db20f73
Magnus Hagander <magnus@hagander.net>
2015-01-18 22:37:00 +0100

    Add support for nickserv

--- bots/initial.py
+++ bots/initial.py
@@ -6,12 +6,30 @@ from prejoin import PreJoin
 
 class Initial(BaseStage):
     def signedOn(self):
-        log.msg("Signed on. Now attempting to join %s." % self.bot.channel)
+        if self.bot.config.has_option('irc', 'nickservpwd'):
+            # Authenticate with nickserv, then wait for that to actually
+            # complete.
+            log.msg("Sending authentication message to nickserv...")
+            self.msg("nickserv", "identify %s %s" % (
+                self.bot.config.get('irc', 'nick'),
+                self.bot.config.get('irc', 'nickservpwd')))
+        else:
+            log.msg("Signed on. Nickserv authentication is disabled.")
+            self.join_channel()
 
-        # Should be set to authenticate instead, but now we will
-        # just join the channel
+    def join_channel(self):
+        log.msg("Attempting to join %s." % self.bot.channel)
+
         self.bot.setStage(PreJoin)
         self.bot.join(self.bot.channel)
 
-    def noticed(slef, user, channel, msg):
-        log.msg("Noticed in %s: %s" % (channel, msg))
+    def noticed(self, user, channel, msg):
+        if user.startswith('NickServ!NickServ@services'):
+            # Message from nickserv! Could be our auth completion
+            if msg.startswith('You are now identified'):
+                log.msg("Nickserv confirms login.")
+                self.join_channel()
+                return
+            log.msg("Message from nickserv: %s" % msg)
+        else:
+            log.msg("Noticed in %s by %s: %s" % (channel, user, msg))
